73be00568bad65f255e0a6d6914edf83f42c1745
ryan <github@rgskinner.com>
2020-10-07 20:18:44 -0700

    trswtdas

--- ryan.py
+++ ryan.py
@@ -1,1 +1,11 @@
-print("hello")
+
+
+def same_necklace(word):
+
+    checklist = []
+    for i in range(len(word)):
+        scramble = word[i+1::] + word[0:i]
+        checklist.append(scramble)
+    print(checklist)
+
+same_necklace('Nicole')
